d1d1442d748235df03133ba267d862a0d7584978
David Lam <davidlam@Davids-MacBook-Pro.local>
2021-07-15 16:00:38 +0800

    creating dataframes

--- dataframes/dataframes.R
+++ dataframes/dataframes.R
@@ -5,3 +5,14 @@ USPersonalExpenditure
 women
 
 data()
+
+# create a dataframes
+days = c('Mon','Tue','Wed','Thur','Fri')
+temp = c(22.2,23,34,25.6,31)
+rain = c(T,T,F,F,T)
+df = data.frame(days,temp,rain)
+df
+
+str(df)
+dim(df)
+summary(df)
